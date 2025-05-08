1a5c0a5dbd2a297f0f13382a227b44d9e55e9cd4
vlsisys <woongchoi@sm.ac.kr>
2025-05-09 00:42:50 +0900

    comment

--- rtl/encode.v
+++ rtl/encode.v
@@ -55,41 +55,35 @@ module encode
 		endcase
 	end
 
-	always @(posedge i_clk or negedge i_rstn) begin
-		if (!i_rstn) begin
-			o_obytes_valid	<= 0;
-		end else begin
-			case(c_state)
-				S_COMP	: o_obytes_valid	<= 1;
-				default	: o_obytes_valid	<= 0;
-			endcase
-		end
-	end
 
 // --------------------------------------------------
 //	Flow Control
 // --------------------------------------------------
-//	coeff			rem
-//	 1	-->  2		: 0 byte & 2-bit
-//	 4	-->  8		: 1 byte & 0-bit
-//	 5	--> 10		: 1 byte & 2-bit
-//	10	--> 20		: 2 byte & 4-bit
-//	11	--> 22		: 2 byte & 6-bit
-//	12	--> 24		: 3 byte & 0-bit
-
-	reg			[5:0]		cnt_obytes;
 	reg			[5:0]		cnt_coeffs;
-	reg			[5:0]		cnt_coeffs_max;
-	reg			[6:0]		offset_base;
-	reg			[6:0]		offset;
+	reg			[5:0]		cnt_obytes;
+	reg			[5:0]		cnt_obytes_max;
 
-	always @(*) begin
-		case (i_l)
-			4'd1	,
-			4'd4	:	offset_base	= 0;
-			4'd11	:	offset_base	= 9;
-			default	:	offset_base	= 4;
-		endcase
+	always @(posedge i_clk or negedge i_rstn) begin
+		if (!i_rstn) begin
+			{o_obytes_valid, cnt_coeffs} <= 0;
+		end else begin
+			case (c_state)
+				S_IDLE		, 
+				S_DONE		: {o_obytes_valid, cnt_coeffs}	<= 0;
+				S_COMP		: begin
+					case (i_l)
+						4'd1	:	{o_obytes_valid, cnt_coeffs} <= cnt_coeffs +  2 >= 64 ? {1, cnt_coeffs +  2 - 64} : {0, cnt_coeffs +  2};
+						4'd4	:	{o_obytes_valid, cnt_coeffs} <= cnt_coeffs +  8 >= 64 ? {1, cnt_coeffs +  8 - 64} : {0, cnt_coeffs +  8};
+						4'd5	:	{o_obytes_valid, cnt_coeffs} <= cnt_coeffs + 10 >= 64 ? {1, cnt_coeffs + 10 - 64} : {0, cnt_coeffs + 10};
+						4'd10	:	{o_obytes_valid, cnt_coeffs} <= cnt_coeffs + 20 >= 64 ? {1, cnt_coeffs + 20 - 64} : {0, cnt_coeffs + 20};
+						4'd11	:	{o_obytes_valid, cnt_coeffs} <= cnt_coeffs + 22 >= 64 ? {1, cnt_coeffs + 22 - 64} : {0, cnt_coeffs + 22};
+						4'd12	:	{o_obytes_valid, cnt_coeffs} <= cnt_coeffs + 24 >= 64 ? {1, cnt_coeffs + 24 - 64} : {0, cnt_coeffs + 24};
+						default	:	{o_obytes_valid, cnt_coeffs} <= 0;
+					endcase
+				end
+				default		: {o_obytes_valid, cnt_coeffs}	<= 0;
+			endcase
+		end
 	end
 
 	always @(*) begin
@@ -106,61 +100,37 @@ module encode
 
 	always @(posedge i_clk or negedge i_rstn) begin
 		if (!i_rstn) begin
-			offset	<= 0;
+			cnt_obytes	<= 0;
 		end else begin
 			case (c_state)
-				S_IDLE		: offset	<= 0;
-				S_COMP_0	,
-				S_COMP_1	: offset	<= offset + offset_base > 64 ? offset - (64 - offset_base) : offset + offset_base;
-				default		: offset	<= offset;
+				S_IDLE	: cnt_obytes <= 0;
+				S_COMP	: cnt_obytes <= o_obytes_valid ? cnt_obytes + 1 : cnt_obytes;
+				default	: cnt_coeffs <= cnt_obytes;
 			endcase
 		end
 	end
 
+	reg		[63:0]		obytes;
 	always @(posedge i_clk or negedge i_rstn) begin
-		if (!i_rstn) begin
-			cnt_ibytes	<= 0;
+		if(!i_rstn) begin
+			obytes	<= 0;
 		end else begin
-			case (c_state)
-				S_IDLE		, 
-				S_DONE		: cnt_ibytes	<= 0;
-				S_COMP_1	: cnt_ibytes	<= cnt_ibytes + 1;
-				default		: cnt_ibytes	<= cnt_ibytes;
+			case (i_l)
+				4'd1	:  obytes[63-cnt_coeffs-: 2] <= {i_coeffs[12], 
+													     i_coeffs[ 0]};
+				4'd4	:  obytes[63-cnt_coeffs-: 8] <= {i_coeffs[12], i_coeffs[13], i_coeffs[14], i_coeffs[15], 
+													     i_coeffs[ 0], i_coeffs[ 1], i_coeffs[ 2], i_coeffs[ 3]};
+				4'd5	:  obytes[63-cnt_coeffs-:10] <= {i_coeffs[12], i_coeffs[13], i_coeffs[14], i_coeffs[15], i_coeffs[16], 
+													     i_coeffs[ 0], i_coeffs[ 1], i_coeffs[ 2], i_coeffs[ 3], i_coeffs[ 4]};
+				4'd10	:  obytes[63-cnt_coeffs-:20] <= {i_coeffs[12], i_coeffs[13], i_coeffs[14], i_coeffs[15], i_coeffs[16], i_coeffs[17], i_coeffs[18], i_coeffs[19], i_coeffs[20], i_coeffs[21], 
+													     i_coeffs[ 0], i_coeffs[ 1], i_coeffs[ 2], i_coeffs[ 3], i_coeffs[ 4], i_coeffs[ 5], i_coeffs[ 6], i_coeffs[ 7], i_coeffs[ 8], i_coeffs[ 9]};
+				4'd11	:  obytes[63-cnt_coeffs-:22] <= {i_coeffs[12], i_coeffs[13], i_coeffs[14], i_coeffs[15], i_coeffs[16], i_coeffs[17], i_coeffs[18], i_coeffs[19], i_coeffs[20], i_coeffs[21], i_coeffs[22], 
+													     i_coeffs[ 0], i_coeffs[ 1], i_coeffs[ 2], i_coeffs[ 3], i_coeffs[ 4], i_coeffs[ 5], i_coeffs[ 6], i_coeffs[ 7], i_coeffs[ 8], i_coeffs[ 9], i_coeffs[10]};
+				4'd12	:  obytes[63-cnt_coeffs-:24] <= {i_coeffs[12], i_coeffs[13], i_coeffs[14], i_coeffs[15], i_coeffs[16], i_coeffs[17], i_coeffs[18], i_coeffs[19], i_coeffs[20], i_coeffs[21], i_coeffs[22], i_coeffs[23], 
+														 i_coeffs[ 0], i_coeffs[ 1], i_coeffs[ 2], i_coeffs[ 3], i_coeffs[ 4], i_coeffs[ 5], i_coeffs[ 6], i_coeffs[ 7], i_coeffs[ 8], i_coeffs[ 9], i_coeffs[10], i_coeffs[11]};
+				default	:  obytes <= obytes;
 			endcase
 		end
-	end
-
-	always @(posedge i_clk or negedge i_rstn) begin
-		if (!i_rstn) begin
-			cnt_coeffs	<= 0;
-		end else begin
-			case (c_state)
-				S_COMP_0	, 
-				S_COMP_1	: cnt_coeffs	<= cnt_coeffs < cnt_coeffs_max ? cnt_coeffs + 1 : cnt_coeffs;
-				default		: cnt_coeffs	<= 0;
-			endcase
-		end
-	end
-
-	wire	[11:0]		coeff_rev[0:1];
-
-	assign	coeff_rev[0] = {i_coeffs[12], i_coeffs[13], i_coeffs[14], i_coeffs[15],
-							i_coeffs[16], i_coeffs[17], i_coeffs[18], i_coeffs[19],
-							i_coeffs[20], i_coeffs[21], i_coeffs[22], i_coeffs[23]};
-
-	assign	coeff_rev[1] = {i_coeffs[ 0], i_coeffs[ 1], i_coeffs[ 2], i_coeffs[ 3],
-							i_coeffs[ 4], i_coeffs[ 5], i_coeffs[ 6], i_coeffs[ 7],
-							i_coeffs[ 8], i_coeffs[ 9], i_coeffs[10], i_coeffs[11]};
-
-	always @(*) begin
-		case (i_l)
-			4'd1	:  = ;
-			4'd4	:  = ;
-			4'd10	:  = ;
-			4'd11	:  = ;
-			4'd12	:  = ;
-			default	:  = ;
-		endcase
 	end
 
 
